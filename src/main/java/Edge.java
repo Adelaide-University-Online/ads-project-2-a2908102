544d6fe1cebbb20366c7ec272733c0e89433ef03
a2908102 <a2908102@adelaide.edu.au>
2026-05-31 18:14:00 +1000

    Added methods to compare edges and get attributes.

--- src/main/java/Edge.java
+++ src/main/java/Edge.java
@@ -8,4 +8,35 @@ public class Edge {
         this.source_vertex = source;
         this.weight = 1.0;
     }
+
+    public boolean equal_edges(Edge edge) {
+        int edge_dest = edge.getDest_vertex();
+        int edge_source = edge.getSource_vertex();
+        if (edge_dest == dest_vertex && edge_source == source_vertex) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int getDest_vertex() {
+        return dest_vertex;
+    }
+
+    public int getSource_vertex() {
+        return source_vertex;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    public String toString() {
+        return super.toString();
+    }
 }
+
