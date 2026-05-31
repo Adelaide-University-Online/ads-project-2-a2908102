6618e83df590c2b71948b46135e967aff3736d78
a2908102 <a2908102@adelaide.edu.au>
2026-05-31 20:30:32 +1000

    Added a graph attribute.

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,4 +1,5 @@
-import java.io.File;
+import java.io.*;
+import java.util.*;
 
 /**
 * File: filename.java
@@ -13,6 +14,7 @@ import java.io.File;
 public class Runner {
     private File file;
     private Integer num_courses;
+    private Graph graph;
 
     Runner(File file, Integer num_courses) {
         this.file = file;
