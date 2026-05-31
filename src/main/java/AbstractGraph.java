3ab0ef78987b3169722002b50c54396881f6fb1a
a2908102 <a2908102@adelaide.edu.au>
2026-05-31 18:20:16 +1000

    Created abstract graph class and added attributes and a constructor.

---
+++ src/main/java/AbstractGraph.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public abstract class AbstractGraph implements Graph {
+    private int num_vertices;
+    private boolean is_directed;
+
+    public AbstractGraph(int num_vertices, boolean is_directed) {
+        this.num_vertices = num_vertices;
+        this.is_directed = is_directed;
+    }
+}
