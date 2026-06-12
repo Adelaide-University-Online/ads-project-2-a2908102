36aff065ba87b482a81bfca65cc613eadba5fdd1
a2908102 <a2908102@adelaide.edu.au>
2026-06-12 10:35:20 +1000

    Added method to get a list of vertices

--- src/main/java/Graph.java
+++ src/main/java/Graph.java
@@ -3,6 +3,7 @@ import java.util.*;
 public interface Graph {
     int getNum_vertices();
     boolean getIs_directed();
+    String[] getVertices();
     void insert_edge(Edge edge);
     boolean is_edge(int source, int destination);
     Edge get_edge(int source, int destination);
